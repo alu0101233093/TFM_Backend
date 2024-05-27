9eb4e07d2fe47edc3d817e4a8cbd7678ed5de20d
Leonardo Alfonso Cruz Rodríguez <alu0101233093@ull.edu.es>
2024-05-27 15:52:10 +0200

    installing dotenv

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,30 @@
-export const PORT = 3000
-export const BASE_URL = 'https://api.themoviedb.org'
-export const MOVIE_URL = BASE_URL + '/3/search/movie'
-export const LANGUAGE_QUERY = '?language=es-ES'
-export const FB_BUCKET_URL = 'miw-tfm-moviemeter.appspot.com'
-export const FB_IMAGE_URL_PREFIX = 'https://firebasestorage.googleapis.com/v0/b/miw-tfm-moviemeter.appspot.com/o/users%2F'
-export const FB_IMAGE_URL_SUFIX = '.png?alt=media'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const PORT = parseInt(process.env.PORT || '3000', 10)
+export const BASE_URL = process.env.BASE_URL || 'https://api.themoviedb.org'
+export const MOVIE_URL = process.env.MOVIE_URL || `${BASE_URL}/3/search/movie`
+export const LANGUAGE_QUERY = process.env.LANGUAGE_QUERY || '?language=es-ES'
+export const FB_BUCKET_URL = process.env.FB_BUCKET_URL || 'miw-tfm-moviemeter.appspot.com'
+export const FB_IMAGE_URL_PREFIX = process.env.FB_IMAGE_URL_PREFIX || 'https://firebasestorage.googleapis.com/v0/b/miw-tfm-moviemeter.appspot.com/o/users%2F'
+export const FB_IMAGE_URL_SUFIX = process.env.FB_IMAGE_URL_SUFIX || '.png?alt=media'
 
 export const DEFAULT_MOVIE_REQUEST = {
-    include_adult: 'true',
-    language: 'es-ES'
+    include_adult: process.env.DEFAULT_MOVIE_REQUEST_INCLUDE_ADULT === 'true',
+    language: process.env.DEFAULT_MOVIE_REQUEST_LANGUAGE || 'es-ES'
 }
 
 export const MOVIE_API_HEADERS = {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MmE0ODc1OTIwOWI0MmNmZmMzZjQ5OTc1MmRhODdlMCIsInN1YiI6IjY2M2I4YTY1OWYxZTYwNDM4ZTA0NWNkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uNfVCc4C57I5gYrc9soA6_8sGA_PkC6zEo6T2wOGuXE'
+    accept: process.env.MOVIE_API_ACCEPT || 'application/json',
+    Authorization: process.env.MOVIE_API_AUTHORIZATION || ''
 }
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCfY0tF__7HF8SOFtFi9YsvGEmgWSeZ3p0",
-    authDomain: "miw-tfm-moviemeter.firebaseapp.com",
-    databaseURL: "https://miw-tfm-moviemeter-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "miw-tfm-moviemeter",
-    storageBucket: "miw-tfm-moviemeter.appspot.com",
-    messagingSenderId: "1052045701013",
-    appId: "1:1052045701013:web:32d386bec0cf6ec25a509c"
-  };
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+}
